5b3112682c5d05cdecbc9c1140244fa00e483d20
Ole Gunnar Nedrebø <olendr@gmail.com>
2017-11-26 01:09:12 +0100

    Define meta inside view

--- viewmodels/news.go
+++ viewmodels/news.go
@@ -20,12 +20,14 @@ type newsView struct {
 // News returns the viewmodel for /news
 func News() newsView {
 	view := newsView{
-		Meta:          DefaultMeta,
+		Meta: meta{
+			Title:       "News - ESA Marathon",
+			Description: "We constantly update with news about our events.",
+			Image:       DefaultMeta.Image,
+		},
 		CopyrightYear: time.Now().Year(),
 		Livemode:      config.Config.LiveMode,
 	}
-	view.Meta.Title = "News - ESA Marathon"
-	view.Meta.Description = "We constantly update with news about our events."
 
 	return view
 }
