125cfbc8ef5906bea9348a2577ccf1bb4e79be65
Johan "Zephyyrr" Fogelström <the@excellent.ninja>
2019-12-06 00:05:16 +0100

    Added permissive CORS header to API requests.

--- handlers/api/router.go
+++ handlers/api/router.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -8,5 +10,13 @@ import (
 Registers the API routes to
 */
 func RegisterRoutes(router *mux.Router) {
+	router.Use(setCORSHeader)
 	router.HandleFunc("/news", News).Methods("GET")
+}
+
+func setCORSHeader(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
 }
